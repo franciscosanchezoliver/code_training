195a8e6bce42c5b53860206ba10990a7f3ec259a
Francisco Sanchez Oliver <spuzi>
2022-08-27 06:31:59 +0200

    longest-palindromic-substring.py

--- leetcode/longest-palindromic-substring.py
+++ leetcode/longest-palindromic-substring.py
@@ -1,27 +1,50 @@
+# https://leetcode.com/problems/longest-palindromic-substring/
+
+from turtle import left, right
+
 
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        print("hello")
-        is_palindrome = self.is_palindrome(s)
 
-    def is_palindrome(self, s):
-        left_pointer = 0
-        right_pointer = len(s)
+        longest_palindrome = ""
+        size_longest_palindrome = 0
 
-        for index in range(len(s)//2):
-            left_char = s[index]
-            right_char = s[-index - 1]
+        for i in range(len(s)):
 
-            if (left_char != right_char): 
-                return False
+            # odd case
+            left_char, right_char = i, i
+            while left_char >= 0 and right_char < len(s) and s[left_char] == s[right_char]:
+                size_of_new_palindrome = right_char - left_char + 1
+                if size_of_new_palindrome > size_longest_palindrome:
+                    size_longest_palindrome = size_of_new_palindrome 
+                    longest_palindrome = s[left_char:right_char + 1]
+                left_char -= 1
+                right_char += 1
 
-        return True
+            # even case
+            left_char, right_char = i, i + 1
+            while left_char >= 0 and right_char < len(s) and s[left_char] == s[right_char]:
+                size_of_new_palindrome = right_char - left_char + 1
+                if size_of_new_palindrome >= size_longest_palindrome:
+                    size_longest_palindrome = size_of_new_palindrome 
+                    longest_palindrome = s[left_char:right_char + 1]
+                left_char -= 1
+                right_char += 1
+
+        return longest_palindrome
 
 
 if __name__ == "__main__":
     solution = Solution()
 
-    #s = "babad"
-    s = "ababa"
+    s = "babad"
     result = solution.longestPalindrome(s)
-    assert(result == "bab")
+    assert result == "bab"
+
+   # s = "cbbd"
+   # result = solution.longestPalindrome(s)
+   # assert result == "bb"
+
+    s = "ccd"
+    result = solution.longestPalindrome(s)
+    assert result == "cc"
