a20c9f0c24c0411d1e844926e2f97fd8366099a1
Francisco Sanchez Oliver <spuzi@hotmail.es>
2024-02-10 08:28:09 +0100

    WIP Adapter pattern in Java

---
+++ design_patterns/adapter/example02/Example02AdapterPattern.java
@@ -0,0 +1,11 @@
+
+public class Example02AdapterPattern {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Adapter example ");
+    }
+
+}
